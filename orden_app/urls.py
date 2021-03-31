81552c70dd842925a413c48fe07c21d922442c53
kraudy <robkraudy@gmail.com>
2021-03-31 09:53:52 -0600

    Creacion vista /lista/

--- orden_app/urls.py
+++ orden_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 from . import views
+from orden_app.views import OrdenListView
+
+app_name = "orden_app"
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('lista/', OrdenListView.as_view(), name='orden_lista'),
 ]
 
 
--- orden_app/views.py
+++ orden_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from django.views.generic.list import ListView
+from .models import Proceso, Orden, Ordenes_procesos
 
 def index(request):
     return render(request, 'orden_app/index.html', {})
 
+class OrdenListView(ListView):
+    model = Orden
 
