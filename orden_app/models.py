fd6edb8602d9eb855227efbf39bc0f1f4e6587cc
kraudy <robkraudy@gmail.com>
2021-03-31 08:16:24 -0600

    Inicializacion

---
+++ orden_app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Proceso(models.Model):
+    nombre = models.CharField(max_length=100)
+    def __str__(self):
+        return self.nombre
+
+class Orden(models.Model):
+    nombre = models.CharField(max_length=100)
+    proceso = models.ManyToManyField(Proceso, verbose_name='lista de procesos')
+    def __str__(self):
+        return self.nombre
+
+class Ordenes_procesos(models.Model):
+    proceso = models.ForeignKey(Proceso, on_delete=models.CASCADE)
+    orden = models.ForeignKey(Orden, on_delete=models.CASCADE)
+    nota = models.CharField(max_length=100)
+    fecha = models.DateTimeField()
