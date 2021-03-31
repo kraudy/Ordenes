5c85a9bfd4fd4afe67c5b50a64786e9fc1d684e1
kraudy <robkraudy@gmail.com>
2021-03-31 10:38:50 -0600

    Agregando vista detalle

--- orden_app/urls.py
+++ orden_app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from . import views
-from orden_app.views import OrdenListView
+from orden_app.views import OrdenListView, OrdenDetailView
 
 app_name = "orden_app"
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('lista/', OrdenListView.as_view(), name='orden_lista'),
+    path('lista/<int:pk>/', OrdenDetailView.as_view(), name='orden_detalle'),
 ]
 
 
--- orden_app/views.py
+++ orden_app/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-from django.views.generic.list import ListView
+
 from .models import Proceso, Orden, Ordenes_procesos
+
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 def index(request):
     return render(request, 'orden_app/index.html', {})
 
 class OrdenListView(ListView):
+    model = Orden
+
+class OrdenDetailView(DetailView):
     model = Orden
 
